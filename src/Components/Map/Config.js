a67945ca6359cc6c57aff2ae7e781d4704e15680
Brenda Berenice Ramos Hernández <elcosmossuena@gmail.com>
2020-01-12 13:13:40 -0600

    se oculto api de google maps

--- src/Components/Map/Config.js
+++ src/Components/Map/Config.js
@@ -1,4 +1,4 @@
 export var config = {
     MY_KEY: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyC5MktJlULerOZZM3lh1tJRaRyQAkxbSh4&v&v=3.exp&libraries=geometry,drawing,places',
   }
-export var apiKey = process.env.APIKEY || 'HF33o9o-gl444olk-992ks';
+
