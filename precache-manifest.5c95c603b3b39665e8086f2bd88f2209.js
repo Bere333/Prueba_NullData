bb55a180492529ed9fa8654c26e1ce7428f70e46
Brenda Berenice Ramos Hernández <elcosmossuena@gmail.com>
2020-01-12 01:47:52 -0600

    Updates

---
+++ precache-manifest.5c95c603b3b39665e8086f2bd88f2209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb2d621ce0e948a5fd823d95f11c66b",
+    "url": "/Prueba_NullData/index.html"
+  },
+  {
+    "revision": "bbd62e901d1b4b12da3b",
+    "url": "/Prueba_NullData/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4a44e001a599d0bac1d2",
+    "url": "/Prueba_NullData/static/js/2.8801b968.chunk.js"
+  },
+  {
+    "revision": "d333f8c97e0d1d6d49ba1f8537021257",
+    "url": "/Prueba_NullData/static/js/2.8801b968.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bbd62e901d1b4b12da3b",
+    "url": "/Prueba_NullData/static/js/main.7d14a412.chunk.js"
+  },
+  {
+    "revision": "943c47592768b989b394",
+    "url": "/Prueba_NullData/static/js/runtime-main.28630668.js"
+  }
+]);
--- src/Components/CreateData.jsx
+++
@@ -1,5 +0,0 @@
-
-export function createData(name, email, birthday, direction, numext, colony, state) {
-    return { name, email, birthday, direction, numext, colony, state};
-  }
-export const rows = []
