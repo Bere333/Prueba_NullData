59c7ff6d380ef7f428be3fa49a90de46972764a2
Brenda Berenice Ramos Hernández <elcosmossuena@gmail.com>
2020-01-12 11:49:09 -0600

    se arreglo privacidad de API google maps

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -8,10 +8,11 @@ import {
   Marker
 } from "react-google-maps";
 
+import {config} from './Config'
+
 export const MyMapComponent = compose(
   withProps({
-    googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyC5MktJlULerOZZM3lh1tJRaRyQAkxbSh4&v&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL:config.MY_KEY,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />
